e9f9e09dc9741620a64468a515c8a7cf69f7ab8a
422UR4H <tiagosindra@id.uff.br>
2023-08-15 15:27:41 -0300

    :sparkles: feat/signIn

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
-import { createUserDB } from "../repositories/user.repository.js";
+import { createUserDB, getUserByEmailDB } from "../repositories/user.repository.js";
 
 dotenv.config();
 
@@ -21,26 +21,26 @@ export async function signUp(req, res) {
     }
 }
 
-// export async function signIn(req, res) {
-//     const { email, password } = req.body;
-//     try {
-//         const result = await getUserByEmailDB(email);
-//         if (result.rowCount === 0) {
-//             return res.status(404).send({ message: "E-mail não cadastrado!" });
-//         }
+export async function signIn(req, res) {
+    const { email, password } = req.body;
+    try {
+        const result = await getUserByEmailDB(email);
+        if (result.rowCount === 0) {
+            return res.status(404).send({ message: "E-mail não cadastrado!" });
+        }
 
-//         const user = result.rows[0];
-//         if (!bcrypt.compareSync(password, user.password)) {
-//             return res.status(401).send({ message: "Senha incorreta!" });
-//         }
+        const user = result.rows[0];
+        if (!bcrypt.compareSync(password, user.password)) {
+            return res.status(401).send({ message: "Senha incorreta!" });
+        }
 
-//         const token = jwt.sign(
-//             { id: user.id },
-//             process.env.JWT_SECRET || "test",
-//             { expiresIn: 24 * 60 * 60 }
-//         );
-//         res.send({ token });
-//     } catch ({ message }) {
-//         res.status(500).send({ message });
-//     }
-// }
+        const token = jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET || "test",
+            { expiresIn: 24 * 60 * 60 }
+        );
+        res.send({ token });
+    } catch ({ message }) {
+        res.status(500).send({ message });
+    }
+}
