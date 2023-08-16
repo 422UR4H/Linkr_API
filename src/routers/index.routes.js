f10d358ef0d64f80e93dde3e761049fdc062c0e9
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-08-16 11:25:51 -0300

    timeline router fixed

--- src/routers/index.routes.js
+++ src/routers/index.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth.routes.js";
 import usersRouter from "./users.routes.js";
+import timelineRoutes from "./timeline.routes.js";
 
 const router = Router();
 
 router.use(authRouter);
+router.use(timelineRoutes);
 router.use(usersRouter);
 
 export default router;
