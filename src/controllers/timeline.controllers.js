49a367ca509e44d3f13a7c5762ccb8c010b21309
Schwartz <darlanswtz@gmail.com>
2023-08-16 19:47:54 -0300

    Feat: timeline posts response comming with likes count and metadatas

--- src/controllers/timeline.controllers.js
+++ src/controllers/timeline.controllers.js
@@ -1,11 +1,32 @@
+import urlMetadata from "url-metadata";
 import { clientDB } from "../database/db.connection.js";
 
 export async function getPosts(req, res) {
   try {
     const getPosts = await clientDB.query(
-      `SELECT * FROM posts ORDER BY id DESC`
+      `SELECT p.*, l.likes_count
+      FROM posts p
+      LEFT JOIN (
+          SELECT posts.id AS post_id, COUNT(l.id) AS likes_count
+          FROM posts 
+          LEFT JOIN likes l ON posts.id = l.liked_post_id
+          GROUP BY posts.id
+      ) AS l ON p.id = l.post_id
+      ORDER BY p.id DESC;
+      `
     );
-    res.status(200).send(getPosts.rows);
+
+    const posts = getPosts.rows;
+    for (const post of posts) {
+      try {
+          const metadata = await urlMetadata(post.link);
+          post.metadata = {description: metadata.description, title: metadata['og:title'],image: metadata['og:image']};
+      } catch (err) {
+          console.log(err);
+      }
+    }
+
+    res.status(200).send(posts);
   } catch (err) {
     console.log(err);
   }
