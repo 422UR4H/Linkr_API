492a7ef52a2898602970760b4c9cf07876c5ffac
Guilherme Oliveira <121904094+GuiOliveiraRamos@users.noreply.github.com>
2023-08-16 20:50:31 -0300

    feat: post likes

--- src/controllers/timeline.controllers.js
+++ src/controllers/timeline.controllers.js
@@ -19,15 +19,35 @@ export async function getPosts(req, res) {
     const posts = getPosts.rows;
     for (const post of posts) {
       try {
-          const metadata = await urlMetadata(post.link);
-          post.metadata = {description: metadata.description, title: metadata['og:title'],image: metadata['og:image']};
+        const metadata = await urlMetadata(post.link);
+        post.metadata = {
+          description: metadata.description,
+          title: metadata["og:title"],
+          image: metadata["og:image"],
+        };
       } catch (err) {
-          console.log(err);
+        console.log(err);
       }
     }
 
     res.status(200).send(posts);
   } catch (err) {
     console.log(err);
+  }
+}
+
+export async function postLike(req, res) {
+  const { id } = req.params;
+  const { like_owner_id } = req.body;
+
+  try {
+    await clientDB.query(
+      `INSERT INTO likes (liked_post_id, like_owner_id) VALUES ($1, $2) `,
+      [id, like_owner_id]
+    );
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Erro ao adicionar o like");
   }
 }
--- src/routers/timeline.routes.js
+++ src/routers/timeline.routes.js
@@ -5,5 +5,6 @@ import { getPosts } from "../controllers/timeline.controllers.js";
 const timelineRoutes = Router();
 
 timelineRoutes.get("/timeline", getPosts);
+timelineRoutes.post("/like/:id");
 
 export default timelineRoutes;
