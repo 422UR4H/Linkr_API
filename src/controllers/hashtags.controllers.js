2348e4ba1cc653606c50a55c928c6213eb3ca580
matheusbruno23 <matheus.bruno.1805@gmail.com>
2023-08-17 19:53:57 -0300

    end-point trending-hashtags funcionando

--- src/controllers/hashtags.controllers.js
+++ src/controllers/hashtags.controllers.js
@@ -1,26 +1,65 @@
-import {db} from "../database/db.connection.js"
+import  {clientDB}  from "../database/db.connection.js"
 
-export async function getPostsByHashtag(req, res){
+export async function getPostsByHashtag(req, res) {
 
-    const {name} = req.body
+
+
     try {
-
-        const AllPostsHashtags = await db.query(`SELECT hash_tags FROM posts;`)
-       // AllPostsHashtags.rows.map()
-
-        const getPosts = await db.query(`SELECT * FROM posts WHERE hash_tags=$1;`, [name])
-
-        res.send(AllPostsHashtags.rows)
-
+        const getTrendings = await clientDB.query(`SELECT * FROM posts WHERE hash_tags=$1;`[hash_tags])
     } catch (error) {
         console.log(error.message)
+        res.sendStatus(500)
     }
 }
 
-export async function getTrendingHashtags(req , res){
+export async function getTrendingHashtags(req, res) {
+
     try {
-        const getTrendings = await db.query(`SELECT * FROM posts WHERE hash_tags > 0;`)
+        let cleanHashtags = []
+        let allHashtagsArray = []
+        let allHashtagsStrings = ''
+        const allPosts = await clientDB.query(`SELECT * FROM posts;`)
+
+        allPosts.rows.forEach(post => {
+            const hashtagString = post.hash_tags
+            if (hashtagString) {
+                allHashtagsStrings += hashtagString + ","
+            }
+        })
+
+        allHashtagsArray = allHashtagsStrings.split(',').map(h => h.trim())
+
+        const sortedHashtags = sortByFrequency(allHashtagsArray)
+        sortedHashtags.forEach( hash => {
+            if(hash !== ""){
+                cleanHashtags.push(hash.replace('#' , ""))
+            }
+            
+        })
+
+        return res.send(cleanHashtags)
     } catch (error) {
         console.log(error.message)
+        res.sendStatus(500)
     }
 }
+
+function sortByFrequency(hashtags) {
+    const frequencyMap = {};
+
+    hashtags.forEach(hashtag => {
+      if (frequencyMap[hashtag]) {
+        frequencyMap[hashtag]++;
+      } else {
+        frequencyMap[hashtag] = 1;
+      }
+    });
+
+    const hashtagFrequencyPairs = Object.entries(frequencyMap);
+
+    hashtagFrequencyPairs.sort((a, b) => b[1] - a[1]);
+
+    const sortedHashtags = hashtagFrequencyPairs.map(pair => pair[0]);
+  
+    return sortedHashtags;
+  }
--- src/routers/timeline.routes.js
+++ src/routers/timeline.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 // import validateAuth from "../middlewares/validateAuth";
 import { getPosts } from "../controllers/timeline.controllers.js";
+import { getTrendingHashtags } from "../controllers/hashtags.controllers.js";
 
 const timelineRoutes = Router();
 
 timelineRoutes.get("/timeline", getPosts);
-timelineRoutes.get("/timeline", getTrendingHashtags)
+
 
 export default timelineRoutes;
