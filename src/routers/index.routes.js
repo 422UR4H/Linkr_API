78ae5c64e9674d0d710011f0db92fff127461c9e
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-08-17 00:24:00 -0300

    feat: create, edit and delete post implementation

--- src/routers/index.routes.js
+++ src/routers/index.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRouter from "./auth.routes.js";
 import usersRouter from "./users.routes.js";
 import timelineRoutes from "./timeline.routes.js";
+import postRouter from "./post.routes.js";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(timelineRoutes);
 router.use(usersRouter);
+router.use(postRouter);
 
 export default router;
---
+++ src/schemas/post.schema.js
@@ -0,0 +1,14 @@
+import joi from "joi";
+
+export const postSchema = joi.object({
+    link: joi.string().uri().required(),
+    description: joi.string(),
+    hash_tags: joi.string()
+});
+
+export const editPostSchema = joi.object({
+    id: joi.string().required(),
+    link: joi.string().uri().required(),
+    description: joi.string(),
+    hash_tags: joi.string()
+});
