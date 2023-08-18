a23b16ec682457addd3808bc4374daa9f6d8594e
matheusbruno23 <matheus.bruno.1805@gmail.com>
2023-08-18 00:34:34 -0300

    validateAuth inseiro em routes hashtag

--- src/routers/users.routes.js
+++ src/routers/users.routes.js
@@ -4,7 +4,7 @@ import validateAuth from "../middlewares/validateAuth.js";
 
 const usersRouter = Router();
 
-usersRouter.get("/user/:id",validateAuth,getUserInfo);
+usersRouter.get("/user/:id",validateAuth, getUserInfo);
 usersRouter.get("/users/:name",validateAuth,getUsersWithName);
 
 export default usersRouter;
