09116f17911327bea85c95e67b313fbffbbc0d6b
matheusbruno23 <matheus.bruno.1805@gmail.com>
2023-08-17 23:31:03 -0300

    objeto hashtag corrigido e resolução de bugs

--- src/routers/timeline.routes.js
+++ src/routers/timeline.routes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
 // import validateAuth from "../middlewares/validateAuth";
-import { getPosts } from "../controllers/timeline.controllers.js";
 import { deleteLike, getPosts, postLike } from "../controllers/timeline.controllers.js";
 import validateAuth from "../middlewares/validateAuth.js";
 const timelineRoutes = Router();
 
-timelineRoutes.get("/timeline", getPosts);
 
 timelineRoutes.post("/like/:id",validateAuth,postLike);
 timelineRoutes.delete("/dislike/:id",validateAuth,deleteLike);
