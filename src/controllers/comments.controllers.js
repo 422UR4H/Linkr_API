dbfd9f8750e57c45ffa6a3411f1808f973ffb77c
422UR4H <tiagosindra@id.uff.br>
2023-08-24 19:35:22 -0300

    :sparkles: feat/createComment

--- src/controllers/comments.controllers.js
+++ src/controllers/comments.controllers.js
@@ -1,14 +1,24 @@
-import createCommentDB from "../repositories/comment.repository.js";
+import { createCommentDB, getCommentsByPostDB } from "../repositories/comment.repository.js";
+
 
 export async function createComment(req, res) {
     const { comment } = req.body;
     const { post_id } = req.params;
     const { id } = res.locals.user;
-
     try {
         const result = await createCommentDB(id, post_id, comment);
         if (result.rowCount === 0) return res.sendStatus(409);
         res.status(201).send(result.rows[0]);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function getCommentsByPost(req, res) {
+    const { post_id } = req.params;
+    try {
+        const result = await getCommentsByPostDB(post_id);
+        res.send(result.rows);
     } catch (err) {
         res.status(500).send(err.message);
     }
--- src/repositories/comment.repository.js
+++ src/repositories/comment.repository.js
@@ -1,6 +1,6 @@
 import { clientDB } from "../database/db.connection.js";
 
-export default function createCommentDB(writer_id, post_id, comment) {
+export function createCommentDB(writer_id, post_id, comment) {
     return clientDB.query(
         `INSERT INTO comments
             (writer_id, post_id, comment)
@@ -11,4 +11,12 @@ export default function createCommentDB(writer_id, post_id, comment) {
     );
 }
 
-// export default function getComments
+export function getCommentsByPostDB(post_id) {
+    return clientDB.query(
+        `SELECT c.*, u.user_name
+        FROM comments AS c
+        JOIN users AS u ON c.writer_id = u.id
+        WHERE post_id = $1`,
+        [post_id]
+    );
+}
