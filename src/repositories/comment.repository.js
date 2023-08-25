3f56ff4811dd8e21ca7718917466ccabcb5cb724
422UR4H <tiagosindra@id.uff.br>
2023-08-25 03:35:32 -0300

    :sparkles: feat/statusFlag

--- src/repositories/comment.repository.js
+++ src/repositories/comment.repository.js
@@ -11,12 +11,21 @@ export function createCommentDB(writer_id, post_id, comment) {
     );
 }
 
-export function getCommentsByPostDB(post_id) {
+export function getCommentsByPostDB(post_id, user_id) {
     return clientDB.query(
-        `SELECT c.*, u.user_name
+        // `SELECT c.*, u.user_name
+        // FROM comments AS c
+        // JOIN users AS u ON c.writer_id = u.id
+        // WHERE c.post_id = $1`,
+        `SELECT c.*, u.user_name, u.photo,
+            CASE
+                WHEN f.following IS NOT NULL THEN TRUE
+                ELSE FALSE
+            END AS is_follower
         FROM comments AS c
         JOIN users AS u ON c.writer_id = u.id
-        WHERE post_id = $1`,
-        [post_id]
+        LEFT JOIN followers AS f ON c.writer_id = f.follower AND f.following = $2
+        WHERE c.post_id = $1`,
+        [post_id, user_id]
     );
 }
